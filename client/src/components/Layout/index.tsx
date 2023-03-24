19e66b3188a152d71f1a10abe6c95306a651a3fb
Yang-ah <yangah0409@gmail.com>
2023-03-25 02:35:44 +0900

    fetched api in layout

--- client/src/components/Layout/index.tsx
+++ client/src/components/Layout/index.tsx
@@ -2,11 +2,11 @@ import { Apple, Dark, Peach, Tree } from "../../theme";
 import styles from "./layout.module.scss";
 import Nav from "./Nav";
 import styled, { ThemeProvider } from "styled-components";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Header from "./Header";
-import { useLocation } from "react-router-dom";
 import Footer from "./Footer";
-// import Logo from "./Logo";
+import { Outlet, useLocation } from "react-router-dom";
+import axios from "axios";
 
 const Wrap = styled.div`
   > main {
@@ -25,6 +25,7 @@ const Layout = () => {
   const [theme, setTheme] = useState(Apple);
   const location = useLocation();
   const homePath = location.pathname === "/";
+  const [data, setData] = useState({});
 
   const onChangeTheme = (theme: string) => {
     theme === "tree" && setTheme(Tree);
@@ -33,12 +34,25 @@ const Layout = () => {
     theme === "dark" && setTheme(Dark);
   };
 
+  const thisMonth = new Date().getMonth();
+
+  const getItems = async () => {
+    const response = await axios.get("http://localhost:4000/api/planner");
+    setData(response.data);
+  };
+
+  useEffect(() => {
+    getItems();
+  }, []);
+
   return (
     <ThemeProvider theme={theme}>
       <Wrap className={styles.wrap}>
-        <main>{!homePath && <Header />}</main>
+        <main>
+          {!homePath && <Header />}
+          <Outlet context={{ data }} />
+        </main>
         <aside>
-          {/*!homePath && <Logo className={styles.logo} />*/}
           <Nav
             onClick={onChangeTheme}
             icon={theme.ICON}
--- client/src/pages/Photo/index.tsx
+++ client/src/pages/Photo/index.tsx
@@ -1,5 +1,33 @@
+import { useState } from "react";
+import { useOutletContext } from "react-router-dom";
+import { ChevronDown } from "../../assets/icon";
+import cx from "classnames";
+import styles from "./photo.module.scss";
+interface PhotoProps {
+  data: {
+    planner: any;
+  };
+}
+
+// TODO : 이번 달 photoUrl만 받아오기. month 조절은 layout에서 하기 (chevron layout으로 이동)
+
 const Photo = () => {
-  return <main></main>;
+  const year = new Date().getFullYear();
+  const month = new Date().getMonth();
+  const [photoMonth, setMonth] = useState(month);
+
+  const monthStr = new Date(year, photoMonth, 1).toLocaleString("en-US", {
+    month: "long",
+  });
+
+  const { data } = useOutletContext<PhotoProps>();
+  console.log("photo", monthStr, data.planner[monthStr]);
+  return (
+    <main>
+      <ChevronDown className={cx(styles.icon, styles.left)} />
+      <ChevronDown className={cx(styles.icon, styles.right)} />
+    </main>
+  );
 };
 
 export default Photo;
